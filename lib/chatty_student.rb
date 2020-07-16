2f06857cf9e75f9ac241990073cd650c1051b8f6
Ryna Truong <rynatruong@outlook.com>
2020-07-16 16:56:54 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,4 +2,13 @@ class ChattyStudent < Student
   
   def initialize()
   end
+  
+  def hello
+    super
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  
+  def raise_hand
+    puts "Pick me!"
+  end
 end
